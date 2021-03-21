1bccad18c5f7248ca155fc39997e837c97276519
yalanin <yalanin@gmail.com>
2021-03-21 10:49:04 +0800

    error message setting

--- app/controllers/admin/vendors_controller.rb
+++ app/controllers/admin/vendors_controller.rb
@@ -7,6 +7,10 @@ class Admin::VendorsController < Admin::BaseController
 
   def new
     @vendor = Vendor.new
+    if flash[:params].present?
+      @vendor.assign_attributes(flash[:params])
+      @vendor.valid?
+    end
   end
 
   def create
@@ -14,7 +18,10 @@ class Admin::VendorsController < Admin::BaseController
     if @vendor.save
       redirect_to admin_vendors_path, notice: '新增成功'
     else
-      render :new
+      # render :new
+      flash[:error] = @vendor.errors.full_messages
+      flash[:params] = vendor_params.to_h
+      redirect_to new_admin_vendor_path
     end
   end
 
@@ -24,7 +31,9 @@ class Admin::VendorsController < Admin::BaseController
     if @vendor.update(vendor_params)
       redirect_to edit_admin_vendor_path(@vendor), notice: '更新成功'
     else
-      render :edit
+      # render :edit
+      flash[:error] = @vendor.errors.full_messages
+      redirect_to edit_admin_vendor_path(@vendor)
     end
   end
 
