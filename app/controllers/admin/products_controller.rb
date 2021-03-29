521b708c8beb0e51d8f71aa6f297ed07bb8218c4
yalanin <yalanin@gmail.com>
2021-03-30 00:03:47 +0800

    user can edit and destroy product

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,8 @@
 class Admin::ProductsController < Admin::BaseController
+  before_action :find_product, only: %i[edit update destroy]
+
   def index
+    @products = Product.includes(:vendor).all
   end
 
   def new
@@ -17,16 +20,27 @@ class Admin::ProductsController < Admin::BaseController
     end
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
+    if @product.update(product_params)
+      redirect_to edit_admin_product_path(@product)
+    else
+      flash[:error] = @product.errors.full_messages
+      redirect_to edit_admin_product_path(@product)
+    end
   end
 
   def destroy
+    @product.destroy
+    redirect_to admin_products_path, notice: '商品已刪除'
   end
 
   private
+
+  def find_product
+    @product = Product.friendly.find(params[:id])
+  end
 
   def product_params
     params.require(:product).permit(:name, :vendor_id, :list_price, :sell_price, :on_sell)
