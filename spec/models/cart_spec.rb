09aa5a12880952095b76ccf05c2193c9ef216f48
yalanin <yalanin@gmail.com>
2021-04-28 22:06:34 +0800

    calculate total price of cart

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe Cart, type: :model do
     # let(:vendor) { FactoryBot.build(:vendor) }
     # let(:category) { FactoryBot.build(:category) }
     let(:product) { create(:product) }
+    let(:product1) { create(:product, sell_price: 5) }
+    let(:product2) { create(:product, sell_price: 10) }
 
     it '商品丟進購物車' do
       cart.add_item(2)
@@ -22,6 +24,12 @@ RSpec.describe Cart, type: :model do
     it '可從購物車拿出商品' do
       cart.add_item(product.id)
       expect(cart.items.first.product).to be_a Product
+    end
+
+    it '計算總金額' do
+      3.times { cart.add_item(product1.id) }
+      2.times { cart.add_item(product2.id) }
+      expect(cart.total_price).to eq(35)
     end
   end
 
