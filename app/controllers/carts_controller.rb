93ca10e5ee4dc162f34fca2b9c69952211867472
yalanin <yalanin@gmail.com>
2021-05-11 21:29:18 +0800

    cart list setting

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,10 @@
 class CartsController < ApplicationController
   before_action :authenticate_user!
 
-  def show
+  def show; end
+
+  def destroy
+    session[:cart_9876] = nil
+    redirect_to root_path, notice: '購物車已清空'
   end
 end
