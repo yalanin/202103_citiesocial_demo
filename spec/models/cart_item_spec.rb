b96e01d538de82650efc04a13acc672bd07c8f93
yalanin <yalanin@gmail.com>
2023-01-23 23:07:50 +0800

    cart_item spec

--- spec/models/cart_item_spec.rb
+++ spec/models/cart_item_spec.rb
@@ -5,6 +5,18 @@ RSpec.describe CartItem, type: :model do
   let(:product1) { create(:product, :with_skus, sell_price: 5) }
   let(:product2) { create(:product, :with_skus, sell_price: 10) }
 
+  it '增加商品數量' do
+    cart.add_sku(product1.id, 2, product1.skus.first.id)
+    expect(cart.items[0].quantity).to eq(2)
+    cart.items[0].increment!(5)
+    expect(cart.items[0].quantity).to eq(7)
+  end
+
+  it '檢查商品 id' do
+    cart.add_sku(product1.id, 1, product1.skus.first.id)
+    expect(cart.items[0].product_id).to eq(1)
+  end
+
   it '計算購物車金額' do
     3.times { cart.add_sku(product1.id, 1, product1.skus.first.id) }
     2.times { cart.add_sku(product2.id, 1, product2.skus.first.id) }
