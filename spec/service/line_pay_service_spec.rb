235d4ec803b178256a4d6e55803ad4537c9706c8
yalanin <yalanin@gmail.com>
2023-02-06 00:00:54 +0800

    line service test

---
+++ spec/service/line_pay_service_spec.rb
@@ -0,0 +1,72 @@
+require 'rails_helper'
+require './spec/share/shared_examples_spec.rb'
+
+RSpec.describe LinePayService, type: :class do
+  let(:line_pay) { double() }
+
+  context 'instance method' do
+    describe '#pay_now' do
+      it 'should get payment url' do
+        allow(line_pay).to receive(:line_pay_request).and_return(pay_success)
+        expect(line_pay.line_pay_request[:returnCode]).to eq('0000')
+        expect(line_pay.line_pay_request[:info][:paymentUrl][:web]).to eq('https://linepay_url')
+      end
+
+      include_examples 'line pay response'
+    end
+
+    describe '#confirm' do
+      it 'should get orderId and transactionId' do
+        allow(line_pay).to receive(:line_pay_request).and_return(confirm_success)
+        expect(line_pay.line_pay_request[:returnCode]).to eq('0000')
+        expect(line_pay.line_pay_request[:info][:orderId]).to eq('AB1234')
+        expect(line_pay.line_pay_request[:info][:transactionId]).to eq('XY9999')
+      end
+
+      include_examples 'line pay response'
+    end
+
+    describe '#cancel' do
+      it 'should get success response' do
+        allow(line_pay).to receive(:line_pay_request).and_return(cancel_success)
+        expect(line_pay.line_pay_request[:returnCode]).to eq('0000')
+      end
+
+      include_examples 'line pay response'
+    end
+  end
+
+  private
+
+  def pay_success
+    {
+      returnCode: '0000',
+      info: {
+        paymentUrl: {
+          web: 'https://linepay_url'
+        }
+      }
+    }
+  end
+
+  def confirm_success
+    {
+      returnCode: '0000',
+      info: {
+        orderId: 'AB1234',
+        transactionId: 'XY9999'
+      }
+    }
+  end
+
+  def cancel_success
+    { returnCode: '0000' }
+  end
+
+  def failed_response
+    {
+      returnCode: '1000',
+      returnMessage: 'something went wrong'
+    }
+  end
+end
--- spec/share/shared_examples_spec.rb
+++ spec/share/shared_examples_spec.rb
@@ -3,3 +3,10 @@ RSpec.shared_examples 'full scope' do
     expect(subject.size).to eq(2)
   end
 end
+
+RSpec.shared_examples 'line pay response' do
+  it 'should get error message' do
+    allow(line_pay).to receive(:line_pay_request).and_return(failed_response)
+    expect(line_pay.line_pay_request[:returnMessage]).to eq('something went wrong')
+  end
+end
